2d08ab5f43a82657352104fd50708843122d43ce
valera <v752433@icloud.com>
2020-04-19 18:21:20 +0300

    admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from main_app import models
+
+
+admin.site.register(models.Student)
+admin.site.register(models.Teacher)
+admin.site.register(models.Specialty)
 
 # Register your models here.
